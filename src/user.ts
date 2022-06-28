50546e0e2d77e660841e70bc4c6df9ba342729a5
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-28 22:19:15 +1000

    Add useful type guard functions to user.ts

--- src/user.ts
+++ src/user.ts
@@ -1,7 +1,12 @@
 import { errorOrData, get } from '.';
-import { UserResponse } from '../types';
+import { Player, User, UserLocation, UserResponse } from '../types';
 
 /** Get a User. */
 export async function getUser(user: string) {
 	return get<UserResponse>(`/users/${user}`).then(errorOrData);
 }
+
+/** Type guard to determine if a Player object is embedded from a Leaderboard resource. */
+export function playerIsUser (player: Player): player is User & { rel: "user"; } { return 'id' in player; }
+/** Type guard to check if user.location is null or not. */
+export function userLocationNotNull (user: User): user is User & { location: UserLocation; } { return user.location !== null; }
