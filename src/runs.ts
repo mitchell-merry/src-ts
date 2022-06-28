6bea87e05d332c9ca71c0208485110601783c48c
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-28 22:10:06 +1000

    Add function to filter runs by variables.

--- src/runs.ts
+++ src/runs.ts
@@ -1,7 +1,12 @@
 import { paginatedGet } from '.';
-import { RunsParams, RunsResponse } from '../types';
+import { Run, RunsParams, RunsResponse } from '../types';
 
 /** Get ALL runs from a certain endpoint. */
 export async function getAllRuns(options?: RunsParams) {
 	return paginatedGet<RunsResponse>(`/runs`, options);
+}
+
+/** Filter a list of runs by variables */
+export function filterRuns(runs: Run[], variables: Record<string, string> = {}) {
+	return runs.filter(run => !Object.entries(variables).some(([variable, value]) => !(variable in run.values) || run.values[variable] !== value));
 }
