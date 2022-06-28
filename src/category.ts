02bd7751d5dedefe4d1fd64af5e4ae51087d3a13
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-28 22:38:59 +1000

    Add categoryIsFullGame type guard.

--- src/category.ts
+++ src/category.ts
@@ -1,7 +1,12 @@
 import { errorOrData, get } from '.';
-import { CategoryVariablesResponse, CategoryVariablesParams } from '../types';
+import { CategoryVariablesResponse, CategoryVariablesParams, Category } from '../types';
 
 /** Get all the variables associated to a category. */
 export async function getCategoryVariables(category: string, options?: CategoryVariablesParams) {
 	return get<CategoryVariablesResponse>(`/categories/${category}/variables`, options).then(errorOrData);
+}
+
+/** Type guard for category type */
+export function categoryIsFullGame(category: Category): category is Category & { type: 'per-game' } {
+	return category.type === 'per-game';
 }
