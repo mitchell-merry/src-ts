55fc8241d94809141698a24642e4313ea6458203
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-28 22:09:52 +1000

    remove incorrect comment

--- src/runs.ts
+++ src/runs.ts
@@ -1,7 +1,7 @@
 import { paginatedGet } from '.';
 import { RunsParams, RunsResponse } from '../types';
 
-/** Get ALL runs from a certain endpoint. Passing no options results in a 400 error. */
+/** Get ALL runs from a certain endpoint. */
 export async function getAllRuns(options?: RunsParams) {
 	return paginatedGet<RunsResponse>(`/runs`, options);
 }
