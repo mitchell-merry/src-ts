bc18ea2dd89be658a482a16ea0d0735202a15260
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-28 22:42:20 +1000

    Change some formatting

--- src/user.ts
+++ src/user.ts
@@ -1,12 +1,17 @@
 import { errorOrData, get } from '.';
 import { Player, User, UserLocation, UserResponse } from '../types';
 
-/** Get a User. */
+/** Get a User object by their ID. */
 export async function getUser(user: string) {
 	return get<UserResponse>(`/users/${user}`).then(errorOrData);
 }
 
 /** Type guard to determine if a Player object is embedded from a Leaderboard resource. */
-export function playerIsUser (player: Player): player is User & { rel: "user"; } { return 'id' in player; }
+export function playerIsUser (player: Player): player is User & { rel: "user" } {
+	return 'id' in player;
+}
+
 /** Type guard to check if user.location is null or not. */
-export function userLocationNotNull (user: User): user is User & { location: UserLocation; } { return user.location !== null; }
+export function userLocationNotNull (user: User): user is User & { location: UserLocation } { 
+	return user.location !== null; 
+}
--- src/variable.ts
+++ src/variable.ts
@@ -3,4 +3,6 @@ import { Variable } from '../types';
 /** Type guard for checking if the given variable is a subcategory or not.
  * Note: Not sure if this guarantees .values.default to be not null.
  */
-export const variableIsSubcategory = (variable: Variable): variable is Variable & { 'is-subcategory': true } => variable['is-subcategory'];
+export function variableIsSubcategory (variable: Variable): variable is Variable & { 'is-subcategory': true } {
+	return variable['is-subcategory'];
+}
