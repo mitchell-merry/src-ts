72426f0819201b4f8c3ca8b2ade873ff4982a6a2
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-28 23:05:41 +1000

    Add documentation and better descriptions to endpoint functions.

--- src/games.ts
+++ src/games.ts
@@ -1,17 +1,35 @@
 import { errorOrData, get } from '.';
 import { GameCategoriesResponse, GameParams, GameResponse, GameCategoriesParams, GameLevelsParams, GameLevelsResponse } from '../types';
 
-/** Get a Game. */
+/** This will retrieve a single game, identified by its ID. Instead of the game's ID, you can also specify the game's abbreviation.
+ * 
+ * GET /games/{id} https://github.com/speedruncomorg/api/blob/master/version1/games.md#get-gamesid
+ * 
+ * @param game The game's ID or abbreviation.
+ * @param options Optional query paramters to pass to the GET request.
+ */
 export async function getGame(game: string, options?: GameParams) {
 	return get<GameResponse>(`/games/${game}`, options).then(errorOrData);
 }
 
-/** Get all categories that belong to a game. Does not filter by category type. */
+/** This will retrieve all categories of a given game (the id can be either the game ID or its abbreviation). If you need only those applicable to certain levels, use getGameLevels.
+ * 
+ * GET /games/{id}/categories https://github.com/speedruncomorg/api/blob/master/version1/games.md#get-gamesidcategories
+ * 
+ * @param game The game's ID or abbreviation.
+ * @param options Optional query paramters to pass to the GET request.
+ */
 export async function getGameCategories(game: string, options?: GameCategoriesParams) {
 	return get<GameCategoriesResponse>(`/games/${game}/categories`, options).then(errorOrData);
 }
 
-/** Get all levels that belong to a game. */
+/** This will retrieve all levels of a given game (the id can be either the game ID or its abbreviation).
+ * 
+ * GET /games/{id}/levels https://github.com/speedruncomorg/api/blob/master/version1/games.md#get-gamesidlevels
+ * 
+ * @param game The game's ID or abbreviation.
+ * @param options Optional query paramters to pass to the GET request.
+*/
 export async function getGameLevels(game: string, options?: GameLevelsParams) {
 	return get<GameLevelsResponse>(`/games/${game}/levels`, options);
 }
--- src/runs.ts
+++ src/runs.ts
@@ -1,12 +1,17 @@
 import { paginatedGet } from '.';
 import { Run, RunsParams, RunsResponse } from '../types';
 
-/** Get ALL runs from a certain endpoint. */
+/** This will return a list of all runs.
+ * 
+ * GET /runs https://github.com/speedruncomorg/api/blob/master/version1/runs.md#get-runs
+ * 
+ * @param options Optional query paramters to pass to the GET request.
+ */
 export async function getAllRuns(options?: RunsParams) {
 	return paginatedGet<RunsResponse>(`/runs`, options);
 }
 
-/** Filter a list of runs by variables */
+/** Filter a list of runs by variables, as key-value pairs. */
 export function filterRuns(runs: Run[], variables: Record<string, string> = {}) {
 	return runs.filter(run => !Object.entries(variables).some(([variable, value]) => !(variable in run.values) || run.values[variable] !== value));
 }
