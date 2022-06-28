293da369bc7c83962782ed5c43f5b731230b903c
Mitchell Merry <mitchellmerry145@gmail.com>
2022-06-28 22:35:23 +1000

    Add getGameLevels.

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,5 @@
 import { errorOrData, get } from '.';
-import { GameCategoriesResponse, GameParams, GameResponse, GameCategoriesParams } from '../types';
+import { GameCategoriesResponse, GameParams, GameResponse, GameCategoriesParams, GameLevelsParams, GameLevelsResponse } from '../types';
 
 /** Get a Game. */
 export async function getGame(game: string, options?: GameParams) {
@@ -9,4 +9,9 @@ export async function getGame(game: string, options?: GameParams) {
 /** Get all categories that belong to a game. Does not filter by category type. */
 export async function getGameCategories(game: string, options?: GameCategoriesParams) {
 	return get<GameCategoriesResponse>(`/games/${game}/categories`, options).then(errorOrData);
+}
+
+/** Get all levels that belong to a game. */
+export async function getGameLevels(game: string, options?: GameLevelsParams) {
+	return get<GameLevelsResponse>(`/games/${game}/levels`, options);
 }
