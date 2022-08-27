846aa073923953696a1abe34eea4043b4ca5a5ae
Mitchell Merry <mitchellmerry145@gmail.com>
2022-08-27 17:58:47 +1000

    Add delete methods

--- src/runs.ts
+++ src/runs.ts
@@ -1,5 +1,5 @@
-import { errorOrData, get, paginatedGet, post, rawPost } from '.';
-import { PostRun, PostRunResponse, Run, RunResponse, RunsParams, RunsResponse } from '../types';
+import { del, errorOrData, get, paginatedGet, post, rawPost } from '.';
+import { DeleteRunResponse, PostRun, PostRunResponse, Run, RunResponse, RunsParams, RunsResponse } from '../types';
 
 /** This will return a list of all runs.
  * 
@@ -31,8 +31,22 @@ export function filterRuns(runs: Run[], variables: Record<string, string> = {}) 
  * POST /runs https://github.com/speedruncomorg/api/blob/master/version1/runs.md#post-runs
  * 
  * @param run The run to submit.
- * @param key THe API key of the account to submit with. Get it from <https://www.speedrun.com/user/USERNAMEHERE/settings/apikey>. Protect it.
+ * @param key The API key of the account to submit with. Get it from <https://www.speedrun.com/user/USERNAMEHERE/settings/apikey>. Protect it.
 */
 export function submitRun(run: PostRun, key: string) {
 	return post<PostRunResponse>('/runs', { run }, key);
+}
+
+/** This method allows an authenticated user to delete a run. Regular users can only delete their
+ *  	own runs, whereas [global] mods can delete runs by other users as well.
+ * 
+ * Note that this is a destructive action.
+ * 
+ * DELETE /runs https://github.com/speedruncomorg/api/blob/master/version1/runs.md#delete-runs
+ * 
+ * @param id The id of the run to delete.
+ * @param key The API key of the account to submit with. Get it from <https://www.speedrun.com/user/USERNAMEHERE/settings/apikey>. Protect it.
+*/
+export function deleteRun(id: string, key: string) {
+	return del<DeleteRunResponse>(`/runs/${id}`, {}, key).then(errorOrData);
 }
