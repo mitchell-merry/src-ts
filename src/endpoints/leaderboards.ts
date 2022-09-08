fede416a86a10ea597c74c0d187b431fc086b43a
Mitchell Merry <mitchellmerry145@gmail.com>
2022-09-09 09:46:06 +1000

    Add leaderboards endpoint funcs

---
+++ src/endpoints/leaderboards.ts
@@ -0,0 +1,28 @@
+import { LeaderboardParams, LeaderboardResponse } from "../../types";
+import { get, shimData } from "../http";
+
+/** This will return a full-game leaderboard. The game and category can be either IDs (e.g. xldev513) or the respective abbreviations.
+ * 
+ * GET /leaderboards/{game}/category/{category} https://github.com/speedruncomorg/api/blob/master/version1/leaderboards.md#get-leaderboardsgamecategorycategory
+ * 
+ * @param game The game's ID or abbreviation.
+ * @param category The category's ID or abbreviation.
+ * @param queryParams Optional query paramters to pass to the GET request.
+ */
+ export async function getLeaderboard(game: string, category: string, queryParams?: LeaderboardParams) {
+	return get<LeaderboardResponse>(`/leaderboards/${game}/category/${category}`, queryParams).then(shimData);
+}
+
+/** This will return a individual-level leaderboard. The same filtering options as with full-game leaderboards apply.
+ * The game, category and level can be either IDs (e.g. xldev513) or their respective abbreviations.
+ * 
+ * GET /leaderboards/{game}/level/{level}/{category} https://github.com/speedruncomorg/api/blob/master/version1/leaderboards.md#get-leaderboardsgamelevellevelcategory
+ * 
+ * @param game The game's ID or abbreviation.
+ * @param level The level's ID or abbreviation.
+ * @param category The category's ID or abbreviation.
+ * @param queryParams Optional query paramters to pass to the GET request.
+ */
+export async function getLevelLeaderboard(game: string, level: string, category: string, queryParams?: LeaderboardParams) {
+	return get<LeaderboardResponse>(`/leaderboards/${game}/level/${level}/${category}`, queryParams).then(shimData);
+}
