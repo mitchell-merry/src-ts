55178a98561a5e01ab30616664556e64a366a8a1
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-02 13:04:23 +1100

    Type docs on Assets

--- types/other.ts
+++ types/other.ts
@@ -9,18 +9,31 @@ export type RelLink<rel> = Link & {
 export interface Asset { uri: string; }
 export interface NullableAsset { uri: string | null; }
 
+/** The assets for a game / series. */
 export interface Assets {
+	/** The logo for `speedrun.com` in the game's / series' style. The regular `speedrun.com` logo if one is not set. */
 	logo: Asset;
+	/** The cover of the game. Speedrun.com's blank `?` cover if not set. */
 	"cover-tiny": Asset;
+	/** The cover of the game. Speedrun.com's blank `?` cover if not set. */
 	"cover-small": Asset;
+	/** The cover of the game. Speedrun.com's blank `?` cover if not set. */
 	"cover-medium": Asset;
+	/** The cover of the game. Speedrun.com's blank `?` cover if not set. */
 	"cover-large": Asset;
+	/** The small icon for the game. Used on speedrun.com in a user's info page, showing which games they moderate / follow. The speedrun.com 1st place trophy if not set. */
 	"icon": Asset;
+	/** The custom 1st place trophy for a game. The default speedrun.com trophy if not set. */
 	"trophy-1st": Asset;
+	/** The custom 2nd place trophy for a game. The default speedrun.com trophy if not set. */
 	"trophy-2nd": Asset;
+	/** The custom 3rd place trophy for a game. The default speedrun.com trophy if not set. */
 	"trophy-3rd": Asset;
+	/** The custom 1st place trophy for a game. Null if not set. */
 	"trophy-4th": Asset | null;
+	/** The background asset of the game's page. Null if not set. */
 	"background": Asset | null;
+	/** The foreground asset of the game's page. Null if not set. */
 	"foreground": Asset | null;
 }
 
