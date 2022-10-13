3499228d1a5dbd64c80b5c36f5506c4b823529d9
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-14 09:58:55 +1100

    Additional test

--- __tests__/games.test.ts
+++ __tests__/games.test.ts
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import fetch, { Response } from "node-fetch";
 import { getGame } from "../src";
 const VERSION = require('root-require')('package.json').version;
 
@@ -9,13 +9,11 @@ beforeEach(() => { mockedFetch.mockClear(); });
 
 test('makes GET /games/:id request correctly', async () => {
 	// @ts-ignore
-	mockedFetch.mockResolvedValue(Promise.resolve({ json: () => Promise.resolve({"data": {"id":"w6jmm26j" }})} as Response));
-	
+	mockedFetch.mockResolvedValue(new Promise.resolve({ json: () => Promise.resolve({"data": { 'id":"w6jmm26j' }})} as Response));
 	const id = "cuphead";
 	const game = await getGame(id);
 
-
-	expect(fetch).toBeCalledWith(`https://www.speedrun.com/api/v1/games/${id}`, { 
+	expect(mockedFetch).toBeCalledWith(`https://www.speedrun.com/api/v1/games/${id}`, { 
 		method: 'get',
 		headers: {
 			'Host': 'www.speedrun.com',
@@ -24,4 +22,6 @@ test('makes GET /games/:id request correctly', async () => {
 		},
 		body: undefined
 	});
+
+	expect(game.id).toBe('w6jmm26j');
 });
