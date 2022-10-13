b3d4d208d3d3c606c5462e53a2c5f5391d92a616
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-14 01:13:09 +1100

    Add prototype of test, not happy with how fetch is mocked here

---
+++ __tests__/games.test.ts
@@ -0,0 +1,27 @@
+import fetch from "node-fetch";
+import { getGame } from "../src";
+const VERSION = require('root-require')('package.json').version;
+
+jest.mock('node-fetch');
+const mockedFetch = jest.mocked(fetch);
+
+beforeEach(() => { mockedFetch.mockClear(); });
+
+test('makes GET /games/:id request correctly', async () => {
+	// @ts-ignore
+	mockedFetch.mockResolvedValue(Promise.resolve({ json: () => Promise.resolve({"data": {"id":"w6jmm26j" }})} as Response));
+	
+	const id = "cuphead";
+	const game = await getGame(id);
+
+
+	expect(fetch).toBeCalledWith(`https://www.speedrun.com/api/v1/games/${id}`, { 
+		method: 'get',
+		headers: {
+			'Host': 'www.speedrun.com',
+			'Content-Type': 'application/json',
+			'User-Agent': `src-ts/${VERSION}`,
+		},
+		body: undefined
+	});
+});
