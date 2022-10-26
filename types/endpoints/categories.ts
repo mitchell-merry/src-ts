9c631380541d4084c1ef57397be8c01defe00f37
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-27 10:21:16 +1100

    Add category param type embeds

--- types/endpoints/categories.ts
+++ types/endpoints/categories.ts
@@ -4,7 +4,7 @@ import { Data } from "../other";
 
 /** GET /categories/{id} https://github.com/speedruncomorg/api/blob/master/version1/categories.md#get-categoriesid */
 export type CategoryResponse<E extends string = ""> = Data<Category<E>>;
-export type CategoryParams = Embed & Callback;
+export type CategoryParams<E extends string = ""> = Embed<E> & Callback;
 
 /** GET /categories/{id}/variables https://github.com/speedruncomorg/api/blob/master/version1/categories.md#get-categoriesidvariables */
 export type CategoryVariablesResponse = Data<Variable[]>;
@@ -12,9 +12,9 @@ export type CategoryVariablesParams = SortParams<"name" | "mandatory" | "user-de
 
 /** GET /categories/{id}/records https://github.com/speedruncomorg/api/blob/master/version1/categories.md#get-categoriesidrecords */
 export type CategoryRecordsResponse<E extends string = ""> = Paginated<Leaderboard<E>>;
-export type CategoryRecordsParams = {
+export type CategoryRecordsParams<E extends string = ""> = {
 	/** only return the top N places (this can result in more than N runs!); this is set to 3 by default */
 	top?: number;
 	/** when set to a true value, empty leaderboards will not show up in the result */
 	"skip-empty"?: boolean;
-} & Embed & PaginatedParams & Callback;
+} & Embed<E> & PaginatedParams & Callback;
