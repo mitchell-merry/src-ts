2710879f693af738aadbf1a676ef887f31c71e4e
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-27 10:25:09 +1100

    Add series param type embeds

--- types/endpoints/series.ts
+++ types/endpoints/series.ts
@@ -5,19 +5,19 @@ import { GamesParams } from "./games";
 
 /** GET /series https://github.com/speedruncomorg/api/blob/master/version1/series.md#get-series */
 export type SeriesAllResponse<E extends string = ""> = Paginated<Series<E>>;
-export type SeriesAllParams = {
+export type SeriesAllParams<E extends string = ""> = {
 	/** when given, performs a fuzzy search across series names and abbreviations */
 	name?: string;
 	/** when given, performs an exact-match search for this abbreviation */
 	abbreviation?: string;
 	/** moderator ID; when given, only series moderated by that user will be returned */
 	moderator?: string;
-} & SortParams<"name.int" | "name.jap" | "abbreviation" | "created"> & Embed & PaginatedParams & Callback;
+} & SortParams<"name.int" | "name.jap" | "abbreviation" | "created"> & Embed<E> & PaginatedParams & Callback;
 
 /** GET /series/{id} https://github.com/speedruncomorg/api/blob/master/version1/series.md#get-seriesid */
 export type SeriesResponse<E extends string = ""> = Data<Series<E>>;
-export type SeriesParams = Embed & Callback;
+export type SeriesParams<E extends string = ""> = Embed<E> & Callback;
 
 /** GET /series/{id}/games https://github.com/speedruncomorg/api/blob/master/version1/series.md#get-seriesidgames */
 export type SeriesGamesResponse<E extends string = ""> = Paginated<Game<E>>;
-export type SeriesGamesParams = GamesParams;
+export type SeriesGamesParams<E extends string = ""> = GamesParams<E>;
