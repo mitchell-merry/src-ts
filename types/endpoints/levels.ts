ca39d3aee2afa7328d08676c0e58b7b46a2b41af
Mitchell Merry <mitchellmerry145@gmail.com>
2022-10-27 10:23:16 +1100

    Add level param type embeds

--- types/endpoints/levels.ts
+++ types/endpoints/levels.ts
@@ -4,24 +4,24 @@ import { Data } from "../other";
 
 /** GET /levels/{id} https://github.com/speedruncomorg/api/blob/master/version1/levels.md#get-levelsid */
 export type LevelResponse<E extends string = ""> = Data<Level<E>>;
-export type LevelParams = Embed & Callback;
+export type LevelParams<E extends string = ""> = Embed<E> & Callback;
 
 /** GET /levels/{id}/categories https://github.com/speedruncomorg/api/blob/master/version1/levels.md#get-levelsidcategories */
 export type LevelCategoriesResponse<E extends string = ""> = Data<Category<E, "per-level">[]>;
-export type LevelCategoriesParams = {
+export type LevelCategoriesParams<E extends string = ""> = {
 	/** when given, filters (out) misc categories */
 	miscellaneous?: boolean;
-} & SortParams<"name" | "miscellaneous" | "pos"> & Embed & Callback;
+} & SortParams<"name" | "miscellaneous" | "pos"> & Embed<E> & Callback;
 
 /** GET /levels/{id}/variables https://github.com/speedruncomorg/api/blob/master/version1/levels.md#get-levelsidvariables */
 export type LevelVariablesResponse = Data<Variable[]>
-export type LevelVariablesParams = SortParams<"name" | "mandatory" | "user-defined" | "pos"> & Embed & Callback;
+export type LevelVariablesParams = SortParams<"name" | "mandatory" | "user-defined" | "pos"> & Callback;
 
 /** GET /levels/{id}/records https://github.com/speedruncomorg/api/blob/master/version1/levels.md#get-levelsidrecords */
 export type LevelLeaderboardResponse<E extends string = ""> = Paginated<Leaderboard<E>>;
-export type LevelLeaderboardParams = {
+export type LevelLeaderboardParams<E extends string = ""> = {
 	/** only return the top N places (this can result in more than N runs!); this is set to 3 by default */
 	top?: number;
 	/** when set to a true value, empty leaderboards will not show up in the result */
 	"skip-empty"?: boolean;
-} & Embed & PaginatedParams & Callback;
+} & Embed<E> & PaginatedParams & Callback;
