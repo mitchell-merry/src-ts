d21c2b148507464e6455a94ace40adbb65c52c81
Mitchell Merry <mitchellmerry145@gmail.com>
2022-11-18 08:12:07 +1100

    Change to filterApplicableVariables

--- src/util.ts
+++ src/util.ts
@@ -44,12 +44,15 @@ export function filterCategoriesByType<Embed extends string, CT extends Category
 	return categories.filter((c): c is Category<Embed, CT> => c.type === type);
 }
 
-/** Filters a list of variables only for those applicable to the `level` (or full-game only, if `level` is not provided.) */
-export function filterVariablesForLevel(variables: Variable[], level?: string) {
+/** Filters a list of variables only for those applicable to the `level` and `category` (or full-game only, if `level` is not provided.) */
+export function filterApplicableVariables(variables: Variable[], category: string, level?: string) {
 	return variables.filter(v => {
+		if (v.category !== null && v.category !== category)
+			return false;
+
 		if (v.scope.type === "global")
 			return true;
-
+	
 		if (v.scope.type === "full-game" && level === undefined)
 			return true;
 
@@ -58,6 +61,8 @@ export function filterVariablesForLevel(variables: Variable[], level?: string) {
 		
 		if (v.scope.type === "single-level" && v.scope.level === level)
 			return true;
+		
+		return false;
 	});
 }
 
